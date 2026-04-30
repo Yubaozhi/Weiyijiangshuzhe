eb77acb472fe4391aa93bedfd01ee07d44fb654f
Yubaozhi <yubaozhi@hotmail.com>
2026-04-30 18:09:34 +0800

    解决cf部署问题-vibecoding

--- docs/.vitepress/theme/components/confetti.vue
+++ docs/.vitepress/theme/components/confetti.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import confetti from 'canvas-confetti'
+import { onMounted } from 'vue'
 
-/* 纸屑 */
-confetti({
+onMounted(() => {
+  confetti({
     particleCount: 100,
     spread: 170,
     origin: { y: 0.6 },
+  })
 })
-
 </script>
